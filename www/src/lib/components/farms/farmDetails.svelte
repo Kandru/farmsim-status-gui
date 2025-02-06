1f0144b4a1ae5c5c625b2694a9f596f54ac5452f
Kalle <info@kalleminkner.de>
2025-02-06 09:32:27 +0100

    draft: farm details

---
+++ www/src/lib/components/farms/farmDetails.svelte
@@ -0,0 +1,42 @@
+<script>
+	let { farm } = $props();
+	import FarmStatistics from '$lib/components/farms/farmStatistics.svelte';
+</script>
+
+{#if farm}
+	<div class="col-12 ps-0 pe-0">
+		<nav>
+			<div class="nav nav-tabs" id="nav-tab" role="tablist">
+				{#if farm && farm.statistics}
+					<button
+						class="nav-link active"
+						id="nav-{farm.id}-tab"
+						data-bs-toggle="tab"
+						data-bs-target="#nav-{farm.farmId}-statistics"
+						type="button"
+						role="tab"
+						aria-controls="nav-{farm.farmId}-statistics"
+						aria-selected="true">Statistics</button
+					>
+				{/if}
+			</div>
+		</nav>
+	</div>
+	<div class="col-12 bg-body-tertiary">
+		<div class="tab-content" id="nav-tabContent">
+			{#if farm && farm.statistics}
+				<div
+					class="tab-pane show active"
+					id="nav-{farm.farmId}-statistics"
+					role="tabpanel"
+					aria-labelledby="Statistics"
+					tabindex="0"
+				>
+					<FarmStatistics {farm} />
+				</div>
+			{/if}
+		</div>
+	</div>
+{:else}
+	<p>No farm data available.</p>
+{/if}
--- www/src/routes/farms/+page.svelte
+++ www/src/routes/farms/+page.svelte
@@ -1,1 +1,49 @@
-<h1>Farms coming soon</h1>
+<script lang="ts">
+	// imports
+	import { onMount } from 'svelte';
+	import FarmDetails from '$lib/components/farms/farmDetails.svelte';
+	let farmsData: any;
+	// create map after mount
+	onMount(async () => {
+		// get farms data
+		let response = await fetch('/data/farms.json');
+		const data = await response.json();
+		farmsData = Object.values(data);
+	});
+</script>
+
+<div class="row">
+	<div class="col-12 ps-0 pe-0">
+		<nav>
+			<div class="nav nav-tabs" id="nav-tab" role="tablist">
+				{#each farmsData as farm, index}
+					<button
+						class="nav-link {index === 0 ? 'active' : ''}"
+						id="nav-{farm.id}-tab"
+						data-bs-toggle="tab"
+						data-bs-target="#nav-{index}"
+						type="button"
+						role="tab"
+						aria-controls="nav-{index}"
+						aria-selected={index === 0 ? 'true' : 'false'}>{farm.name}</button
+					>
+				{/each}
+			</div>
+		</nav>
+	</div>
+	<div class="col-12 bg-body-tertiary">
+		<div class="tab-content" id="nav-tabContent">
+			{#each farmsData as farm, index}
+				<div
+					class="tab-pane {index === 0 ? 'show' : ''} {index === 0 ? 'active' : ''}"
+					id="nav-{index}"
+					role="tabpanel"
+					aria-labelledby={farm.name}
+					tabindex={index}
+				>
+					<FarmDetails {farm} />
+				</div>
+			{/each}
+		</div>
+	</div>
+</div>
